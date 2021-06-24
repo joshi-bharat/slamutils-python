f9e3e045aadb61b3c6d1649cdd3415560e855347
joshi-bharat <robotics.bharat@gmail.com>
2021-06-24 15:32:24 -0400

    rsaFSA

--- colmap.py
+++ colmap.py
@@ -1,7 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
-from typing import Collection
+
+from numpy import linalg
+from numpy.linalg.linalg import inv
 import pandas as pd
+import numpy as np
 
 from tf.transformations import *
 
@@ -21,9 +24,9 @@ if __name__ == '__main__':
     # Skipping odd lines as we need only camera poses.
     image_infos = lines[::2]
 
-    print("Remaining lines: {}".format(len(image_infos)))
-
     camera_poses = []
+    all_trans = []
+
     for image_info in image_infos:
         if image_info.startswith('#'):
             continue
@@ -35,14 +38,25 @@ if __name__ == '__main__':
         if(int(cam_id) == 1):
             # print(info_array)
             stamp = info_array[9].split('_')[1].split('.')[0]
-            trans = info_array[5:8]
-            quat = 
-            data = [stamp] + 
+            trans = np.array(info_array[5:8], dtype=np.float64)
+            quat = (float(info_array[2]), float(info_array[3]), float(info_array[4]),
+                    float(info_array[1]))  # qx qy qz qw order
+            rotm = quaternion_matrix(quat)[:3, :3]
+            trans_result = - (np.linalg.inv(rotm).dot(trans))
+            # print("result: {}".format(trans_result))
+            data = [stamp] + trans_result.tolist()
             camera_poses.append(data)
+            all_trans.append(trans_result)
         # print(data)
+
+    all_trans = np.array(all_trans)
+    relative_trans = np.diff(all_trans, axis=0)
+    dist = np.linalg.norm(relative_trans, axis=1)
+    result = np.where(dist > 3)
+    print(result)
 
     # print(camera_poses)
     df = pd.DataFrame(camera_poses, columns=['stamp', 'tx', 'ty', 'tz'])
-    # df.sort_values(by=['stamp'])
+    df.sort_values(by=['stamp'])
     df.to_csv('colmap_poses.txt', index=False, header=False)
-    print(df)
+    # print(df)
